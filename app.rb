6038fc84c969d047a7e9a52f0932ca730fd6888f
benediktgirz <benedikt.girz@gmail.com>
2013-11-28 21:24:26 -0800

    Initial API setup, via Sinatra for MVP sake returns JSON, ready to be deployed to heroku

--- app.rb
+++ app.rb
@@ -1,87 +1,22 @@
 require 'sinatra'
+require 'json'
 
-get '/' do
-  "hello world"
+get '/example.json' do
+
+  content_type :json 
+  {results: 
+    
+      {coordinate: [34.7663658, -124.4513667],
+        name: "Wholefoods",
+        product: "PBR",
+        price: "$3.5",
+        discount: "10%",
+        address: "717California blah blah"
+      }
+       
+    
+  }.to_json
 end
 
 
 
-
-
-# require 'dbc-ruby'
-
-# user_fields = [:name, :bio, :challenge_attempts, :cohort, :cohort_id, :email, :exercise_attempts, :id, :profile, :roles]
-# cohort_fields = [:name, :email, :id, :in_session, :location, :start_date, :students]
-# profile_hash = [:github, :quora, :twitter, :facebook, :linked_in, :blog, :hacker_news, :about, :hometown, :current_location]
-
-# # p DBC::Cohort.all.first.methods.sort - Object.methods
-# # p DBC::Profile.methods.sort - Object.methods
-
-# # p DBC::Cohort.all.select(&:in_session).map(&:students) #  .map{|gs| gs.map{|s| s.profile[:about]}}
-
-
-# class Gravatar
-#   DEFAULT = "http://www.gravatar.com/avatar/00000000000000000000000000000000"
-
-#   def self.url_from(email)
-#     return DEFAULT if clean(email).nil? || clean(email).empty?
-#     "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(clean(email))}"
-#   end
-
-#   def self.clean(email)
-#     email.downcase.strip
-#   end
-
-# end
-
-# students = DBC::Cohort.all.select(&:in_session).sample(1).map(&:students)
-
-
-# stObj = "{ 'students':["
-
-# students.map do |gs| 
-#   gs.map do |s| 
-
-#     stObj += "{"
-#     stObj += "'id' : '#{s.id}', "
-#     stObj += "'name' : '#{s.name}', "
-#     stObj += "'email' : '#{s.email}', "
-#     stObj += "'gravatar' :'#{Gravatar.url_from(s.email)}', "
-#     stObj += "'profile' : 'https://socrates.devbootcamp.com/users/#{s.id}'"
-#     stObj += "},"
-#    end
-# end
-
-# stObj = stObj[0..-2]
-# stObj += ']}'
-
-# puts stObj.gsub("'", '"')
-
-# __END__
-
-# personas:
-
-# - current student
-# ? who else is here
-# ? contact my accountability buddies
-# ? announce something
-# ? i am locked out
-# ? 
-
-# - incoming student
-# ? who will be there during my tenure
-#   ? get to know them
-# ? who is there now
-# ? who are the alumni
-# ? tell me more about teachers
-# ? 
-
-# # - alumni
-# ? who is there now
-# ? 
-
-# # - coach
-# # - teacher
-# # - staff
-# # - guest speaker
-# # - recruiter
